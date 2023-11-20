7cf8212e9bf52bcde0896c67240a7b2182ec264d
Harshil Kotadiya <harshilkotadiya002@gmail.com>
2023-11-20 10:41:42 +0530

    init commit

--- apps/my-nest-app/jest.config.ts
+++ apps/my-nest-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'my-workspace',
+  displayName: 'my-nest-app',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/my-workspace',
+  coverageDirectory: '../../coverage/apps/my-nest-app',
 };
