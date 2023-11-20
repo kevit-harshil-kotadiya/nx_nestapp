9ccd1fbf00fe115ef7536f53ae69159de35eb48e
Harshil Kotadiya <harshilkotadiya002@gmail.com>
2023-11-20 17:55:14 +0530

    added e2e testing for app: my-nest-app

---
+++ apps/my-nest-app-e2e/src/my-nest-app/my-nest-app.spec.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+describe('GET /api', () => {
+  it('should return a message', async () => {
+    const res = await axios.get(`/api`);
+
+    console.log('running e2e');
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual({ message: 'Hello API' });
+  });
+});
